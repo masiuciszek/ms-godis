eb81dd7ec326d93820f9f2af2abc30c700dff9e8
Masiu <ciszekmarcell@gmail.com>
2020-03-28 11:38:11 +0100

    add

--- auth/middlewares/roleHandler.js
+++ auth/middlewares/roleHandler.js
@@ -3,42 +3,11 @@ const jwt = require('jsonwebtoken');
 const asyncHandler = require('./asyncHandler');
 const User = require('../models/User');
 
-const auth = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    // Set token from Bearer token in header
-    token = req.headers.authorization.split(' ')[1];
-    // Set token from cookie
-  } else if (req.cookies.token) {
-    token = req.cookies.token;
+const role = asyncHandler(async (req, res, next) => {
+  if (req.user.role !== 'admin') {
+    throw new Error('Not a admin!');
   }
-
-  console.log(token);
-
-  if (!token) {
-    throw new Error('NOT AUTHORIZED!');
-  }
-
-  const decoded = jwt.verify(token, 'secret');
-  const user = await User.findOne({
-    _id: decoded.id,
-    'sessionTokens.token': token,
-  });
-  // 'sessionTokens.token': token
-  if (!user) {
-    throw new Error('NOT AUTHORIZED!');
-  }
-
-  req.user = user;
-  req.token = token;
-  // console.log(decoded);
-  // console.log(token);
-  // console.log('COOKIES', req.cookies);
   next();
 });
 
-module.exports = auth;
+module.exports = role;
--- auth/routes/user.js
+++ auth/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const auth = require('../middlewares/authHandler');
+const role = require('../middlewares/roleHandler');
 
 const router = express.Router();
 const { register, getMe, getAdmin } = require('../controllers/user.controller');
 
 router.route('/register').post(register);
 router.route('/me').get(auth, getMe);
-router.route('/admin').get(auth, getAdmin);
+router.route('/admin').get(auth, role, getAdmin);
 
 module.exports = router;
